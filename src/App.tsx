34d21c6714fe50fd0503980aee60f09788764842
Egor Bazakin <egor.bazakin.09@gmail.com>
2025-04-15 15:26:18 +0300

    Update with TSconfigPathsPlugin

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import '@test/app.ts'
 import '@test/style.scss'
-// import '@test/image.jpg'
+
+import './index.css'
 
 export default () => {
   return (
     <>
+			<div className="spinner"></div>
 			<h1>Webpack Bundling</h1>
-			<img src="image.jpg" alt="example image"></img>
     </>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const PATHS = {
   app:   join(__dirname, 'src/index.js'),
@@ -17,14 +18,7 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'jsx'],
-    alias: {
-      '@components': join(__dirname, 'src/components/'),
-      '@utils':      join(__dirname, 'src/utils/'),  
-      '@pages':      join(__dirname, 'src/pages/'),
-      '@types':      join(__dirname, 'src/types/'),
-      '@context':    join(__dirname, 'src/context/'),
-      '@test':       join(__dirname, 'src/test/'),
-    }
+    plugins: [new TsconfigPathsPlugin()],
   },
   module: {
     rules: [
@@ -73,6 +67,11 @@ module.exports = {
         test: /\.(png|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.svg$/i,
+        issuer: /\.(tsx|jsx)$/,
+        use: ['@svgr/webpack'],
+      },      
     ],
   },
   optimization: {
